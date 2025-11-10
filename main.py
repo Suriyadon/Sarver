419124be0245511d7b21b4dfdf7b97d424107ba9
Suriyadon <Ramkumar2002y@gmail.com>
2025-11-10 08:51:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#ENCODED BY : SAIM TRICKER BRAND
+#ENCODED BY : SURYA TRICKER brand 
 #ENCRYPTION : Py3 ZLIB
 #GITHUB : https://github.com/anaysakour
 #----------------------------------------------
